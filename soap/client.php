948e45941b52fbb60d9359a4b4d2347fcab9239b
Jérôme Duval <jero.duval@gmail.com>
2014-06-11 18:28:49 +0200

    [SOAP] add check + clean data file

--- soap/client.php
+++ soap/client.php
@@ -20,11 +20,24 @@ class EsgiSoapClient
 	 * @param  int  $id_area Area's id which is commented 
 	 * @param  string  $author  The name of the author 
 	 * @param  string  $content  The content of the comment 
-	 * @param  int  $rate  The rate of the area, 1-10 
+	 * @param  int  $rate  The rate of the area, 0-10 
 	 * @return  array  The new comment created with all his values
 	 */
 	public function AddComment($id_area, $author, $content, $rate)
 	{
+		if($id_area == "") {
+			return 'The area id is mandatory!';
+		}
+		if($author == "") {
+			return 'The author is mandatory!';
+		}
+		if($content == "") {
+			return 'The content is mandatory!';
+		}
+		if($rate < 0 || $rate > 10) {
+			return 'The rate value must be between 0 and 10!';
+		}
+
 		$err = $this->client->getError();
 		if ($err) {
 			return $err;
