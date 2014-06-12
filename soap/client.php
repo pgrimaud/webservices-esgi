7caa1acc8cbbed10c2f7f2a1eb026603542081a0
Jérôme Duval <jero.duval@gmail.com>
2014-06-12 11:13:03 +0200

    [SOAP] fix xsd + rename xml file

--- soap/client.php
+++ soap/client.php
@@ -2,75 +2,79 @@
 class EsgiSoapClient
 {
 
-	public $client;
+    public $client;
 
-	public function __construct()
-	{
-		require_once 'lib/nusoap.php';
-		require_once 'lib/config.php';
+    public function __construct()
+    {
+        require_once 'lib/nusoap.php';
+        require_once 'lib/config.php';
 
-		$namespace = URL_WS_SOAP;
-		$this->client = new nusoap_client(URL_WS_SOAP."?wsdl");
-	}
-
-
-	/**
-	 * Add a comment to an area 
-	 *
-	 * @param  int  $id_area Area's id which is commented 
-	 * @param  string  $author  The name of the author 
-	 * @param  string  $content  The content of the comment 
-	 * @param  int  $rate  The rate of the area, 0-10 
-	 * @return  array  The new comment created with all his values
-	 */
-	public function AddComment($id_area, $author, $content, $rate)
-	{
-		if($id_area == "") {
-			return 'The area id is mandatory!';
-		}
-		if($author == "") {
-			return 'The author is mandatory!';
-		}
-		if($content == "") {
-			return 'The content is mandatory!';
-		}
-		if($rate < 0 || $rate > 10) {
-			return 'The rate value must be between 0 and 10!';
-		}
-
-		$err = $this->client->getError();
-		if ($err) {
-			return $err;
-		}
-
-		$result = $this->client->call('AddComment', 
-			array(
-				'id_area'	=> $id_area,
-				'author'	=> $author, 
-				'content'	=> $content, 
-				'rate'		=> $rate
-			)
-		);
-
-		return $result;
-	}
+        $namespace = URL_WS_SOAP;
+        $this->client = new nusoap_client(URL_WS_SOAP."?wsdl");
+    }
 
 
-	/**
-	 * Get all comments of an area
-	 *
-	 * @param  int  $id_area Area's id which is commented 
-	 * @return  array  All comments created with all his values of an area
-	 */
-	public function GetComments($id_area)
-	{
-		$err = $this->client->getError();
-		if ($err) {
-			return $err;
-		}
+    /**
+     * Add a review to an area
+     *
+     * @param   int      $id_area Area's id which is reviewed
+     * @param   string   $author  The name of the author
+     * @param   string   $content  The content of the review
+     * @param   int      $rate  The rate of the area, 1-10
+     * @return  array    The new review created with all his values
+     */
+    public function AddReview($id_area, $author, $content, $rate)
+    {
+        if($id_area == "") {
+            return 'The area id is mandatory!';
+        }
+        if($author == "") {
+            return 'The author is mandatory!';
+        }
+        if($content == "") {
+            return 'The content is mandatory!';
+        }
+        if($rate < 1 || $rate > 10) {
+            return 'The rate value must be between 1 and 10!';
+        }
 
-		$result = $this->client->call("GetComments", array("id_area" => $id_area));
+        $err = $this->client->getError();
+        if ($err) {
+            return $err;
+        }
 
-		return $result;
-	}
+        $result = $this->client->call(
+            "AddReview",
+            array(
+                'id_area'   => $id_area,
+                'author'    => $author,
+                'content'   => $content,
+                'rate'      => $rate
+            )
+        );
+
+        return $result;
+    }
+
+
+    /**
+     * Get all reviews of an area
+     *
+     * @param   int     $id_area Area's id which is reviewed
+     * @return  array   All reviews created with all his values of an area
+     */
+    public function GetReviews($id_area)
+    {
+        $err = $this->client->getError();
+        if ($err) {
+            return $err;
+        }
+
+        $result = $this->client->call(
+            "GetReviews",
+            array("id_area" => $id_area)
+        );
+
+        return $result;
+    }
 }
--- soap/server.php
+++ soap/server.php
@@ -6,119 +6,113 @@ $namespace = URL_WS_SOAP;
 
 $server = new nusoap_server();
 $server->wsdl->schemaTargetNamespace = $namespace;
-// $server->configureWSDL("soap_test", "urn:soap_test");
-$server->configureWSDL('hellotesting');
+$server->configureWSDL('WSDLFTW');
 
 $server->wsdl->addComplexType(
-	'Comment',
-	'complexType',
-	'struct',
-	'all',
-	'',
-	array(
-		'id_area'	=> array('name' => 'id_area', 'type' => 'xsd:int'),
-		'author'	=> array('name' => 'author', 'type' => 'xsd:string'),
-		'content'	=> array('name' => 'content', 'type' => 'xsd:string'),
-		'rate'		=> array('name' => 'rate', 'type' => 'xsd:int'),
-	)
+    'Review',
+    'complexType',
+    'struct',
+    'all',
+    '',
+    array(
+        'id_area' => array('name' => 'id_area', 'type' => 'xsd:int'),
+        'author'  => array('name' => 'author', 'type' => 'xsd:string'),
+        'content' => array('name' => 'content', 'type' => 'xsd:string'),
+        'rate'    => array('name' => 'rate', 'type' => 'xsd:int'),
+    )
 );
 
 $server->wsdl->addComplexType(
-	'GetCommentArray',
-	'complexType',
-	'array',
-	'',
-	'SOAP-ENC:Array',
-	array(),
-	array(
-		array(
-			'ref'			 => 'SOAP-ENC:arrayType',
-			'wsdl:arrayType' => 'tns:Comment[]'
-		)
-	),
-	'tns:Comment'
+    'GetReviewArray',
+    'complexType',
+    'array',
+    '',
+    'SOAP-ENC:Array',
+    array(),
+    array(
+        array(
+            'ref'             => 'SOAP-ENC:arrayType',
+            'wsdl:arrayType' => 'tns:Review[]'
+        )
+    ),
+    'tns:Review'
 );
 
 $server->register(
-	"GetComments",
-	array("id_area" => "xsd:int"),
-	array("return"  => "tns:GetCommentArray"),
-	$namespace,
-	false,
-	"rpc",
-	"encoded",
-	"Get users' comments about the specified area."
+    "GetReviews",
+    array("id_area" => "xsd:int"),
+    array("return" => "tns:GetReviewArray"),
+    $namespace,
+    false,
+    "rpc",
+    "encoded",
+    "Get users' reviews about the specified area."
 );
 
-$server->register('AddComment',
-	array(
-		'id_area'	=> 'xsd:int',
-		'author'	=> 'xsd:string',
-		'content'	=> 'xsd:string',
-		'rate'		=> 'xsd:int'
-	),
-	array('return' => 'tns:Comment'),
-	$namespace,
-	false,
-	'rpc', // style
-	'encoded', // use
-	'Add a comment.'
+$server->register(
+    "AddReview",
+    array(
+        'id_area'   => 'xsd:int',
+        'author'    => 'xsd:string',
+        'content'   => 'xsd:string',
+        'rate'      => 'xsd:int'
+    ),
+    array('return' => 'tns:Review'),
+    $namespace,
+    false,
+    'rpc', // style
+    'encoded', // use
+    'Add a review.'
 );
 
-
-function GetComments($id_area)
+function GetReviews($id_area)
 {
-	$comments = new SimpleXMLElement('data/comments.xml', NULL, TRUE);
-	$result = array();
+    $reviews = new SimpleXMLElement('data/reviews.xml', NULL, TRUE);
+    $result = array();
 
-	foreach($comments->xpath('//comment[id_area="'.$id_area.'"]') as $comment) {
-		$result[] = array(
-			'id_area'	=> (int)	$comment->id_area,
-			'author'	=> (string)	$comment->author,
-			'content'	=> (string)	$comment->content,
-			'rate'		=> (int)	$comment->rate
-		);
-	}
+    foreach($reviews->xpath('//review[id_area="'.$id_area.'"]') as $review) {
+        $result[] = array(
+            'id_area'   => (int)    $review->id_area,
+            'author'    => (string) $review->author,
+            'content'   => (string) $review->content,
+            'rate'      => (int)    $review->rate
+        );
+    }
 
-	return $result;
+    return $result;
 }
 
-function AddComment($id_area, $author, $content, $rate)
+function AddReview($id_area, $author, $content, $rate)
 {
-	$data = file_get_contents('data/comments.xml');
+    $xml = new DOMDocument();
+    $xml->load('data/reviews.xml');
 
-	$xml = new SimpleXMLElement($data);
+    $reviews = $xml->getElementsByTagName('reviews')->item(0);
 
-	$comments = $xml->xpath('//comments');
-	$comments = $comments[0];
+    $review = $xml->createElement('review');
+    $xml->createElement('review');
 
-	$comment = $comments->addChild('comment');
-	$comment->addChild('id_area', $id_area);
-	$comment->addChild('author', $author);
-	$comment->addChild('content', $content);
-	$comment->addChild('rate', $rate);
+    $areaElem    = $xml->createElement('id_area', $id_area);
+    $authorElem  = $xml->createElement('author', $author);
+    $contentElem = $xml->createElement('content', $content);
+    $rateElem    = $xml->createElement('rate', $rate);
 
-	try {
-		file_put_contents('data/comments.xml', $xml->asXML());
-	} catch(Exception $e) {
-		return $e;
-	}
+    $review->appendChild($areaElem);
+    $review->appendChild($authorElem);
+    $review->appendChild($contentElem);
+    $review->appendChild($rateElem);
 
-	$result = array(
-		'id_area'	=> $id_area,
-		'author'	=> $author,
-		'content'	=> $content,
-		'rate'		=> $rate
-	);
+    $reviews->appendChild($review);
 
-	return $result;
+    if($xml->schemaValidate('data/schema.xsd')) {
+        $xml->save('data/reviews.xml');
+    } else {
+        return "Error: input doesn't match xsd schema rules";
+    }
+    return $xml->saveXML($xml);
 }
-
-
 
 // create HTTP listener
-// $HTTP_RAW_POST_DATA = isset($HTTP_RAW_POST_DATA) ? $HTTP_RAW_POST_DATA : '';
 $POST_DATA = isset($GLOBALS['HTTP_RAW_POST_DATA']) ? $GLOBALS['HTTP_RAW_POST_DATA'] : '';
-// $server->service($HTTP_RAW_POST_DATA);
 $server->service($POST_DATA);
 exit();
